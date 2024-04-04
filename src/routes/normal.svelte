82fcad02c81377842b633db501d2e46e4cff816e
xianii <xianyi.xia@outlook.com>
2024-04-04 23:10:47 +0800

    add glyph draw component

--- src/routes/normal.svelte
+++ src/routes/normal.svelte
@@ -1,9 +1,13 @@
+<script>
+	import Glyph from './Glyph.svelte';
+</script>
+
 <div class="grid grid-flow-row gap-4 h-max m-4 justify-center content-center">
 	<ul class="steps">
-		<li data-content="✓" class="step step-success">FIELD</li>
-		<li data-content="✓" class="step step-success">TOGETHER</li>
-		<li data-content="✓" class="step step-success">IMPROVE</li>
-		<li data-content="?" class="step step-neutral">HUMAN</li>
-		<li data-content="?" class="step step-neutral">MIND</li>
+		<li data-content="?" class="step step-neutral"><Glyph glyph="bhcjd-" />FIELD</li>
+		<li data-content="?" class="step step-neutral"><Glyph glyph="lharta" />TOGETHER</li>
+		<li data-content="?" class="step step-neutral"><Glyph glyph="jard" />IMPROVE</li>
+		<li data-content="?" class="step step-neutral"><Glyph glyph="trjch-" />HUMAN</li>
+		<li data-content="?" class="step step-neutral"><Glyph glyph="tach-" />MIND</li>
 	</ul>
 </div>
